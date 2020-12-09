f0e47b1de5de8896bce42ceeee4e8d5dc16bd1ab
prnvpndy <pranav.pandey@successive.tech>
2020-12-09 18:28:14 +0530

    React - Introduction to State

--- training/src/components/Slider/Slider.jsx
+++ training/src/components/Slider/Slider.jsx
@@ -4,54 +4,53 @@ import Img from './style';
 import { getRandomNumber, getNextRoundRobin } from '../../libs/utils/math';
 
 class Slider extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            current: -1,
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: -1,
 
-        };
-    }
+    };
+  }
 
     componentDidMount = () => {
-        const { random, duration } = this.props;
-        let { current } = this.state;
-        this.id = setInterval(() => {
-            if (random) {
-                current = getRandomNumber(total);
-            } else {
-                current = getNextRoundRobin(current, total);
-            }
-            this.setState({ current });
-        }, duration);
+      const { random, duration } = this.props;
+      let { current } = this.state;
+      this.id = setInterval(() => {
+        if (random) {
+          current = getRandomNumber(total);
+        } else {
+          current = getNextRoundRobin(current, total);
+        }
+        this.setState({ current });
+      }, duration);
     }
 
     componentWillUnmount = () => {
-        clearInterval(this.id);
+      clearInterval(this.id);
     }
+
     render() {
-
-        const { current } = this.state;
-        const {
-            altText, height, duration, banner,
-        } = this.props;
-        const { defaultbanner } = this.props;
-        if (current === -1 || banner.length === 0) {
-            return (
-                <>
-                    <div align="center">
-                        <Img src={`${defaultbanner}`} alt={altText} height={height} duration={duration} />
-                    </div>
-                </>
-            );
-        }
+      const { current } = this.state;
+      const {
+        altText, height, duration, banner,
+      } = this.props;
+      const { defaultbanner } = this.props;
+      if (current === -1 || banner.length === 0) {
         return (
-            <>
-                <div align="center">
-                    <Img src={`${PUBLIC_IMAGE_FOLDER}${banner[current]}`} alt={altText} height={height} duration={duration} />
-                </div>
-            </>
+          <>
+            <div align="center">
+              <Img src={`${defaultbanner}`} alt={altText} height={height} duration={duration} />
+            </div>
+          </>
         );
+      }
+      return (
+        <>
+          <div align="center">
+            <Img src={`${PUBLIC_IMAGE_FOLDER}${banner[current]}`} alt={altText} height={height} duration={duration} />
+          </div>
+        </>
+      );
     }
-
 }
 export default Slider;
