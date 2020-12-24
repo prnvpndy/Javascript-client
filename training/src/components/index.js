f8cc64ccf4eff29a1c25e613e0e78031eb84f99a
prnvpndy <pranav.pandey@successive.tech>
2020-12-24 15:57:05 +0530

    React: Table Component

--- training/src/components/index.js
+++ training/src/components/index.js
@@ -4,3 +4,4 @@ export { SelectField } from './SelectField';
 export { RadioField } from './RadioField';
 export { ButtonField } from './ButtonField';
 export { Math } from './Math';
+export { TableComponent } from './Table';
