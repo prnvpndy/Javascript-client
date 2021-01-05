02daed12ffc6b46a763149ef1c8c32107aa01e8c
prnvpndy <pranav.pandey@successive.tech>
2021-01-05 18:01:06 +0530

    Pagination in Table

--- training/src/pages/Trainee/traineeStyle.js
+++ training/src/pages/Trainee/traineeStyle.js
@@ -6,4 +6,35 @@ const useStyles = (theme) => ({
     textAlign: 'right',
   },
 });
-export default useStyles;
+const style = (theme) => ({
+  root: {
+    display: 'flex',
+    height: 160,
+    margin: theme.spacing(5),
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 170,
+    backgroundColor: '#545454',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    marginLeft: theme.spacing(5),
+  },
+  back: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: 'lightgrey',
+    color: 'black',
+    marginLeft: theme.spacing(80),
+  },
+});
+export default { useStyles, style };
